b3adf9c045f92b5c43399a81c224d9a23928df7b
smoawad66 <93600247+smoawad66@users.noreply.github.com>
2024-06-16 13:37:18 +0300

    Update layout-top.php

--- views/partials/layout-top.php
+++ views/partials/layout-top.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes App</title>
     <script src="http://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
 </head>
 
 <body class="h-full text-white" style="background-color: #030607;">
